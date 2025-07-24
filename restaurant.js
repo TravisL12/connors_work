a50a03bdf2f5cc61620ec0e4a02f373347d7d525
Travis Lawrence <travis.lawrence12@gmail.com>
2025-07-24 11:41:17 -0700

    Create restaurant.js

---
+++ restaurant.js
@@ -0,0 +1,40 @@
+```
+Customer type:
+* max spending - total amount of money a customer might spending
+* demand - the faster they expect food/service
+
+
+
+Wealthy Customer
+* max spending - 1000
+* demand - high
+* label - "Wealthy"
+
+Average Customer
+* max spending - 200
+* demand - medium
+* label - "Average"
+
+Low Customer
+* max spending - 75
+* demand - low
+* label - "Low"
+```
+
+const wealthyCustomer = {
+    maxSpending: 1000,
+    demand: 'high',
+    label: 'Wealthy',
+}
+
+const averageCustomer = {
+    maxSpending: 1000,
+    demand: 'medium',
+    label: 'Average',
+}
+
+const lowCustomer = {
+    maxSpending: 1000,
+    demand: 'low',
+    label: 'Low',
+}
